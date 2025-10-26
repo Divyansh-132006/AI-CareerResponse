ec05c3a42c76b5d575626c0ff32f3acb961040fa
Divyansh Upadhyay <udivyansh93@gmail.com>
2025-10-26 16:37:46 +0530

    Enable server functionality and update CORS settings
    Uncommented server setup code and updated CORS origin.

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import aiRoutes from './src/routes/aiRoutes.js'; 
-// import login from './src/routes/auth.js';
-// import signup from './src/routes/auth.js';
-// import resume from './src/routes/ai.js';
-
-
-// dotenv.config();
-  
-
-// const app = express();
-
-// mongoose.connect(process.env.MongoPassword, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log("✅ MongoDB connected");
-// }).catch(err => {
-//   console.error("❌ MongoDB connection error:", err);
-// });
-// app.use(cors({
-//   origin: 'https://darling-bavarois-e52057.netlify.app',
-//   credentials: true
-// }));
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/ai', aiRoutes);
-// app.use('/api/ai/ask', aiRoutes);
-// app.use('/api/ai/', login);
-// app.use('/api/ai/', signup);
-// app.use('/api/ai/', resume);
-
-
-// app.get("/ping", (req, res) => {
-//   res.status(200).send("pong");
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
 
 
 import express from 'express';
@@ -71,7 +25,7 @@ mongoose.connect(process.env.MongoPassword, {
   console.error("❌ MongoDB connection error:", err);
 });
 app.use(cors({
-  origin: 'https://melodic-selkie-5a91c4.netlify.app',
+  origin: 'https://aicareer-advisor.netlify.app/',
   credentials: true
 }));
 // app.use(cors({
